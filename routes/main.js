88faae89e28493f4c9a8fe9a537b24649e496723
Kirupavathi.S <23cse067@act.edu.in>
2026-03-12 11:37:59 +0530

    Updated routes/main.js for debugging

--- routes/main.js
+++ routes/main.js
@@ -1,9 +1,9 @@
-const express=require("express")
-const router=express.Router()
-const controller=require("../controllers/mainController")
+const express = require("express");
+const router = express.Router();
+const controller = require("../controllers/mainController");
 
-router.post("/create",controller.create)
-router.get("/list",controller.list)
-router.delete("/:id",controller.remove)
+router.post("/", controller.create);
+router.get("/", controller.list);
+router.delete("/:id", controller.remove);
 
-module.exports=router
+module.exports = router;
