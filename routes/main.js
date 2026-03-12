30b522c3fc03f1af404a8c483edef0c763816504
SANTHOSH R <23aids044@act.edu.in>
2026-03-12 15:47:22 +0530

    Delete routes directory

--- routes/main.js
+++
@@ -1,9 +0,0 @@
-const express=require("express")
-const router=express.Router()
-const controller=require("../controllers/mainController")
-
-router.post("/create",controller.create)
-router.get("/list",controller.list)
-router.delete("/:id",controller.remove)
-
-module.exports=router
