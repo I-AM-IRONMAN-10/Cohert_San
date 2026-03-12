7c58bea0c8e83d2862b358df8df5b3bf0d789210
Kirupavathi.S <23cse067@act.edu.in>
2026-03-12 11:21:07 +0530

    Debug fix in mainController

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,16 +1,31 @@
-const service=require("../services/mainService")
+const service = require("../services/mainService");
 
-exports.create=(req,res)=>{
-const item=service.create(req.body)
-res.json(item)
-}
+exports.create = (req, res) => {
+    try {
+        if (!req.body.title || !req.body.secret) {
+            return res.status(400).json({ error: "Title and secret are required" });
+        }
+        const item = service.create(req.body);
+        res.status(201).json(item);
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+};
 
-exports.list=(req,res)=>{
-const data=service.list()
-res.json(data)
-}
+exports.list = (req, res) => {
+    try {
+        const data = service.list();
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+};
 
-exports.remove=(req,res)=>{
-service.remove(req.params.id)
-res.json({message:"deleted"})
-}
+exports.remove = (req, res) => {
+    try {
+        service.remove(req.params.id);
+        res.status(200).json({ message: "deleted" });
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+};
